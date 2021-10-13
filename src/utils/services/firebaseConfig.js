cc1d764ba149a83ed7bd262125fc99c515d96717
asad.shabir <asad.shabir@arbisoft.com>
2021-10-13 15:26:26 +0500

    test commit

--- src/utils/services/firebaseConfig.js
+++ src/utils/services/firebaseConfig.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyBZA9oT1gebvIf6Gk6Ih-SFgP0ceb-SJ0o',
   authDomain: 'olx-clone-react-cdf31.firebaseapp.com',
